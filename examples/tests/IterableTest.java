778a34faa213907bcf94c558963d5909ed6d48ad
Anton Haubner <anton.haubner@outlook.de>
2021-12-31 21:17:37 +0100

    Add test for mapping iterables.

---
+++ examples/tests/IterableTest.java
@@ -0,0 +1,15 @@
+package examples.tests;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class IterableTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+
+        System.out.println("Hello Data!");
+    }
+}
