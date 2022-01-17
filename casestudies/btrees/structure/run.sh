305e40129e04cc302f1128a8a68396126df7063e
Anton Haubner <anton.haubner@outlook.de>
2022-01-17 12:15:58 +0100

    factor out case study regarding btree structure

---
+++ casestudies/btrees/structure/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+shopt -s globstar
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
+
+JAVA_VER=$(java -version 2>&1 | sed -n ';s/.* version "\(.*\)\.\(.*\)\..*".*/\1\2/p;')
+if [ "$JAVA_VER" -lt 11 ]
+then
+  echo "You need at least Java 11"
+  exit
+fi
+
+javac -g casestudies/btrees/structure/**/*.java
+./gradlew run --args="--color casestudies/btrees/structure/StructureTest.sjd"
