157ee4c7efa79287bc9eb79c1019b91dd3126d31
Anton Haubner <anton.haubner@outlook.de>
2022-02-13 23:03:08 +0100

    Source path can now be set manually (used to find breakpoint locations by search string)

---
+++ src/main/kotlin/de/ahbnr/semanticweb/java_debugger/repl/commands/SourcePathCommand.kt
@@ -0,0 +1,22 @@
+@file:Suppress("JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE", "JAVA_MODULE_DOES_NOT_DEPEND_ON_MODULE")
+
+package de.ahbnr.semanticweb.java_debugger.repl.commands
+
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.types.path
+import de.ahbnr.semanticweb.java_debugger.logging.Logger
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import java.nio.file.Path
+
+class SourcePathCommand : REPLCommand(name = "sourcepath"), KoinComponent {
+    private val logger: Logger by inject()
+
+    val path: Path by argument().path(mustExist = true, mustBeReadable = true)
+
+    override fun run() {
+        state.sourcePath = path
+
+        logger.success("Source path set.")
+    }
+}
