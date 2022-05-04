0204a0b652169f41436f1fee950c436628b0ccf0
Anton Haubner <anton.haubner@outlook.de>
2022-05-04 14:55:06 +0200

    update doubly-linked performance case study to use new memory measurement method

--- casestudies/performance/DoublyLinkedCaseStudy.py
+++ casestudies/performance/DoublyLinkedCaseStudy.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import Dict, Set, NamedTuple
+from typing import Dict, Set, NamedTuple, Optional
 
 import numpy as np
 import pandas as pd
@@ -10,7 +10,8 @@ from runner import compileProject, runSJDB, retrieveAverageTime, retrieveMemory
 
 starttime = datetime.datetime.now()
 
-low_range_evaluation = True
+low_range_evaluation = False
+infer_evaluation = False
 high_range_evaluation = True
 
 write_results = True
@@ -20,16 +21,51 @@ resultColumns = [
     "memory"
 ]
 
-warmup = 5
-repeat = 4
-timeout = 60
+warmup = 0
+repeat = 1
+timeout = 120
+
+measure_memory = True
+measure_times = False
 
 tasks = ["buildkb", "sparql", "shacl", "infer"]
 
 
 class ExperimentResult(NamedTuple):
-    time: datetime.timedelta
-    memory: int
+    time: Optional[datetime.timedelta]
+    memory: Optional[int]
+
+
+def write_store(base_name: str, results: pd.DataFrame):
+    if write_results:
+        if measure_memory:
+            name = f"{base_name}_memory.h5"
+            with pd.HDFStore(name) as store:
+                store['results'] = results[['memory']]
+                store['meta'] = pd.DataFrame(
+                    np.array([
+                        [
+                            repeat,
+                            warmup,
+                            timeout,
+                        ]
+                    ]),
+                    columns=['repeat', 'warmup', 'timeout']
+                )
+        if measure_times:
+            name = f"{base_name}_times.h5"
+            with pd.HDFStore(name) as store:
+                store['results'] = results[['times']]
+                store['meta'] = pd.DataFrame(
+                    np.array([
+                        [
+                            repeat,
+                            warmup,
+                            timeout,
+                        ]
+                    ]),
+                    columns=['repeat', 'warmup', 'timeout']
+                )
 
 
 def run_single_experiment(
@@ -40,45 +76,61 @@ def run_single_experiment(
     sjdb_script_env["repeat"] = repeat
 
     projectPath = "java/doublylinked"
+    templatePrefix = "DoublyLinked"
 
     render_template(
-        filePath=f"{projectPath}/DoublyLinked.template.java",
-        targetPath=f"{projectPath}/DoublyLinked.java",
+        filePath=f"{projectPath}/{templatePrefix}.template.java",
+        targetPath=f"{projectPath}/{templatePrefix}.java",
         env=java_env
     )
 
     compileProject(projectPath)
 
-    render_template(
-        filePath=f"{projectPath}/DoublyLinked-time.template.sjdb",
-        targetPath=f"{projectPath}/DoublyLinked.sjdb",
-        env=sjdb_script_env
-    )
+    time = None
+    if measure_times:
+        render_template(
+            filePath=f"{projectPath}/{templatePrefix}-time.template.sjdb",
+            targetPath=f"{projectPath}/{templatePrefix}.sjdb",
+            env=sjdb_script_env
+        )
 
-    runSJDB(
-        projectPath=projectPath,
-        taskfile="DoublyLinked.sjdb",
-        timeout=None,
-        monitorMemory=False,
-        printer=tqdm.write
-    )
-    time = retrieveAverageTime(projectPath)
+        runSJDB(
+            projectPath=projectPath,
+            taskfile=f"{templatePrefix}.sjdb",
+            timeout=None,
+            monitorMemory=False,
+            printer=tqdm.write
+        )
+        time = retrieveAverageTime(projectPath)
 
     # memory
-    render_template(
-        filePath=f"{projectPath}/DoublyLinked-memory.template.sjdb",
-        targetPath=f"{projectPath}/DoublyLinked.sjdb",
-        env=sjdb_script_env
-    )
+    memory = None
+    if measure_memory and repeat > 0:
+        lastMemoryUsage: int = 0
+        memory = 0
+        with tqdm(range(0, repeat), total=repeat) as t:
+            for repeatIdx in t:
+                avgUsage = int(memory / repeatIdx / (1024 * 1024)) if repeatIdx > 0 else 0
+                avgUsageStr = f"{avgUsage} MiB"
+                lastUsageStr = f"{int(lastMemoryUsage / (1024 * 1024))} MiB"
+                t.set_description(f"MEASURING MEMORY USE (avg: {avgUsageStr}, last: {lastUsageStr})")
+                render_template(
+                    filePath=f"{projectPath}/{templatePrefix}-memory.template.sjdb",
+                    targetPath=f"{projectPath}/{templatePrefix}.sjdb",
+                    env=sjdb_script_env
+                )
 
-    runSJDB(
-        projectPath=projectPath,
-        taskfile="DoublyLinked.sjdb",
-        timeout=None,
-        monitorMemory=True,
-        printer=tqdm.write
-    )
-    memory = retrieveMemory(projectPath)
+                runSJDB(
+                    projectPath=projectPath,
+                    taskfile=f"{templatePrefix}.sjdb",
+                    timeout=None,
+                    monitorMemory=True,
+                    printer=tqdm.write
+                )
+                lastMemoryUsage = retrieveMemory(projectPath)
+                memory += lastMemoryUsage
+
+        memory /= repeat
 
     return ExperimentResult(
         time=time,
@@ -94,16 +146,18 @@ def experiment_for_each_task(
 ) -> pd.DataFrame:
     times: Dict[str, datetime.timedelta] = dict()
     memory: Dict[str, int] = dict()
-    for task in tasks:
-        if task in excluded_tasks:
-            continue
+    with tqdm(tasks, total=len(tasks)) as t:
+        for task in t:
+            t.set_description(f"TASK {task}")
+            if task in excluded_tasks:
+                continue
 
-        sjdb_script_env['task'] = task
+            sjdb_script_env['task'] = task
 
-        result = run_single_experiment(java_env, sjdb_script_env)
+            result = run_single_experiment(java_env, sjdb_script_env)
 
-        times |= {task: result.time}
-        memory |= {task: result.memory}
+            times |= {task: result.time}
+            memory |= {task: result.memory}
 
     return pd.DataFrame(
         np.array([
@@ -118,13 +172,12 @@ def experiment_for_each_task(
 
 
 if low_range_evaluation:
-    num_nodes_options = [30, 40, 50, 100, 200, 500]
+    num_nodes_options = [30, 40, 50, 100, 200, 300, 400, 500]
     results: pd.DataFrame = pd.DataFrame(np.empty((0, len(resultColumns))), columns=resultColumns)
 
     with tqdm(enumerate(num_nodes_options), total=len(num_nodes_options)) as t:
-        t.set_description("LOW RANGE")
         for i, num_nodes in t:
-            tqdm.write("Evaluating for {} nodes...".format(num_nodes))
+            t.set_description(f"LOW RANGE ({num_nodes} nodes)")
 
             java_env = {
                 "num_nodes": num_nodes,
@@ -134,13 +187,32 @@ if low_range_evaluation:
                 "timeout": timeout,
             }
 
-            tqdm.write(f"LOW RANGE: {i + 1}/{len(num_nodes_options)}")
-
             results = pd.concat([results, experiment_for_each_task([num_nodes], java_env, sjdb_script_env, set())])
 
-    if write_results:
-        with pd.HDFStore('DoublyLinkedStore.h5') as store:
-            store['results'] = results
+    write_store('DoublyLinkedLowStore', results)
+
+    tqdm.write(results.to_string())
+
+if infer_evaluation:
+    num_nodes_options = [500]
+    results: pd.DataFrame = pd.DataFrame(np.empty((0, len(resultColumns))), columns=resultColumns)
+
+    with tqdm(enumerate(num_nodes_options), total=len(num_nodes_options)) as t:
+        for i, num_nodes in t:
+            t.set_description(f"INFER ({num_nodes} nodes)")
+
+            java_env = {
+                "num_nodes": num_nodes,
+            }
+
+            sjdb_script_env = {
+                "timeout": timeout,
+            }
+
+            results = pd.concat(
+                [results, experiment_for_each_task([num_nodes], java_env, sjdb_script_env, set(tasks) - {'infer'})])
+
+    write_store('DoublyLinkedLowStore', results)
 
     tqdm.write(results.to_string())
 
@@ -149,16 +221,13 @@ if high_range_evaluation:
     step_size = 1000
     max_steps = 10
     num_nodes_options = [start + step * step_size for step in range(0, max_steps)]
-    tqdm.write("We will be evaluating for the following node counts:")
-    tqdm.write(num_nodes_options)
 
     results: pd.DataFrame = pd.DataFrame(np.empty((0, len(resultColumns))), columns=resultColumns)
 
     with tqdm(enumerate(num_nodes_options), total=len(num_nodes_options)) as t:
-        t.set_description("HIGH RANGE")
 
         for i, num_nodes in t:
-            tqdm.write("Evaluating for {} nodes...".format(num_nodes))
+            t.set_description(f"HIGH RANGE ({num_nodes} nodes)")
 
             java_env = {
                 "num_nodes": num_nodes,
@@ -168,13 +237,9 @@ if high_range_evaluation:
                 "timeout": timeout,
             }
 
-            tqdm.write(f"HIGH RANGE: {i + 1}/{len(num_nodes_options)}")
-
             results = pd.concat([results, experiment_for_each_task([num_nodes], java_env, sjdb_script_env, {'infer'})])
 
-    if write_results:
-        with pd.HDFStore('DoublyLinkedHighStore.h5') as store:
-            store['results'] = results
+    write_store('DoublyLinkedHighStore', results)
 
     tqdm.write(results.to_string())
 
